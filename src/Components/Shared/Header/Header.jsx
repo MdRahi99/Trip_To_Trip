313584e808b63aa75b36d7dee6d8a3ce3a7d0edb
Md Rahi <fhrfhr102030@gmail.com>
2023-04-13 02:25:42 +0600

    setup navbar

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { Navbar } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdDashboard } from "@react-icons/all-files/md/MdDashboard";
+import { FaArrowAltCircleRight } from "@react-icons/all-files/fa/FaArrowAltCircleRight";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Navbar className="w-screen" fluid={true} rounded={true}>
+      <Navbar.Brand className="bg-slate-900 p-2 text-white" as={Link} to="/">
+        
+        <span className="flex items-center gap-3 self-center whitespace-nowrap text-xl font-semibold">
+          <p>Trip</p> 
+          <FaArrowAltCircleRight></FaArrowAltCircleRight>
+          <p>Trip</p>
+        </span>
+      </Navbar.Brand>
+
+      <div className="flex md:order-2">
+        <Link className="bg-slate-900 text-white p-2 items-center" to="/dashboard">
+          <MdDashboard></MdDashboard>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+
+      <Navbar.Collapse>
+        <Navbar.Link as={Link} to="/" active={true}>
+          Home
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/about">About</Navbar.Link>
+        <Navbar.Link as={Link} to="/contact">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Header;
--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Components/Home/Home/Home";
+import About from "../../Components/About/About";
+import Contact from "../../Components/Contact/Contact";
+import DashBoard from "../../Components/DashBoard/DashBoard/DashBord";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
         {
             path: "/",
             element: <Home></Home>
+        },
+        {
+            path: "/about",
+            element: <About></About>
+        },
+        {
+            path: "/contact",
+            element: <Contact></Contact>
+        },
+        {
+            path: "/dashboard",
+            element: <DashBoard></DashBoard>
         },
     ],
   },
