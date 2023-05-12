3a9aba65a56dddd6f89afc3edccf6b965e9b3f2e
Md Rahi <fhrfhr102030@gmail.com>
2023-05-12 23:19:26 +0600

    edit ui for packages

--- src/Components/Packages/Package.jsx
+++ src/Components/Packages/Package.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 import { RiArrowRightCircleFill } from "@react-icons/all-files/ri/RiArrowRightCircleFill";
 
 const Package = ({ pack }) => {
-  const { id, title, ratings, views } = pack;
+  const { id, title, ratings, views, img } = pack;
 
   return (
     <Link
       to={`/packages/${id}`}
       className="p-4 hover:outline hover:outline-2 hover:outline-slate-900 bg-white text-slate-900 mx-4 my-2"
     >
+      <div className="p-0 m-0">
+        <img className="h-60 w-full mb-6" src={img} alt="" />
+      </div>
       <div className="flex justify-between items-center">
         <div className="flex flex-col gap-3">
           <h2 className="text-2xl font-semibold">{title}</h2>
